766d4a6c7e5ed7d0040d70f16460ac59d1b88ab7
Gregwar <g.passault@gmail.com>
2019-03-21 10:21:04 +0100

    Fix

--- atmega328pb-update.sh
+++ atmega328pb-update.sh
@@ -1,7 +1,7 @@
 echo "deb http://archive.ubuntu.com/ubuntu/ bionic main restricted universe multiverse" > /etc/apt/sources.list.d/tmp.list
 apt-get update
-apt-get install -y gcc-avr
+apt-get install -y gcc-avr avrdude
 rm /etc/apt/sources.list.d/tmp.list
 apt-get update
 cp /etc/avrdude.conf /etc/avrdude_save.conf
-cp misc/avrdue.conf /etc/
+cp misc/avrdude.conf /etc/
