c34ef72fe5ea6d35cba808f18223eaaa19424f9d
Gregwar <g.passault@gmail.com>
2022-04-15 19:05:03 +0200

    There is no led

--- firmware/src/main/led.c
+++ firmware/src/main/led.c
@@ -3,11 +3,11 @@
 
 void led_init()
 {
-    DDRD |= _BV(PD5);
+    // DDRD |= _BV(PD5);
 }
 
 void led_set(int en)
 {
-    if (en) PORTD |= _BV(PD5);
-    if (!en) PORTD &= ~_BV(PD5);
+    // if (en) PORTD |= _BV(PD5);
+    // if (!en) PORTD &= ~_BV(PD5);
 }
